bd51983da42a9f1b033525d6c676061d6b495da0
jamalull <jamaluldazzled@gmail.com>
2023-06-16 21:35:11 +0700

    add initial project for jobit Webb Apps

---
+++ jobit-web-app/src/App.jsx
@@ -0,0 +1,20 @@
+
+// import Footer from './components/Footer'
+import Footer2 from './components/Footer2'
+import Navbar from './components/Navbar'
+
+function App() {
+  
+  return (
+    <>
+    <Navbar/>
+      <h1 className="h-screen text-3xl text-center pt-10 font-bold underline">
+        Hello world!
+      </h1>
+      {/* <Footer/> */}
+      <Footer2/>
+    </>
+  )
+}
+
+export default App
