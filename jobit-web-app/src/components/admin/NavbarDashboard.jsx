4703d3b9964a7373c6164f8ed62f93eedeef7505
jamalull <jamaluldazzled@gmail.com>
2023-06-22 16:30:42 +0700

    update component NavbarDashboard: add Link router and change some text, and logo

--- jobit-web-app/src/components/admin/NavbarDashboard.jsx
+++ jobit-web-app/src/components/admin/NavbarDashboard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function NavbarDashboard() {
   return (
     <>
-      <header>
+      <header className="bg-gray-50">
         <div className="mx-auto max-w-screen-xl bg-gray-50 px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
           <div className="sm:flex sm:items-center sm:justify-between">
             <div className="text-center sm:text-left">
